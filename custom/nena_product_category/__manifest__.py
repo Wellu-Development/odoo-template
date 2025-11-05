2bf11690215d19cdda844b0d04109aaa8e24d86a
Renier Ferrer <renier.ferrer@dronena.com>
2025-11-05 15:37:55 -0400

    [ADD] Inherit module for product category

---
+++ custom/nena_product_category/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Nena Categoría de productos",
+    "summary": "Extensión modelo product.category",
+    "author": "Dronena",
+    "category": "Customization",
+    "version": "18.0.0.0.1",
+    "application": True,
+    "depends": ["stock"],
+    "data": ["views/product_category.xml"],
+}
