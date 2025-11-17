ec9cbcff5fe8e28c9c52242904b5d89e25ac6506
SIST-JALVAREZ <jose.alvarez@dronena.com>
2025-11-17 09:47:08 -0400

    Configuracion de Zonas y Disponibilidad del Dpto. de Cobranza

---
+++ custom/nena_client_assignment/models/__init__.py
@@ -0,0 +1,5 @@
+from . import nena_config_collection_management
+from . import nena_hr_department
+from . import nena_gen_category_inheritance
+from . import nena_hr_job
+from . import nena_hr_employee
