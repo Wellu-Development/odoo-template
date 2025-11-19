55bea5acc08d44899cbbd6de177b8e37a633dc6a
Anais Chavez <anais.chavez@dronena.com>
2025-11-19 08:06:28 -0400

    [MERGE] nena_drug_composition: Se integraron cambios realizados por el equipo para ficha de producto

--- custom/nena_client_assignment/models/nena_hr_department.py
+++ custom/nena_client_assignment/models/nena_hr_department.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class NenaHrDepartment(models.Model):
     _inherit = "hr.department"
 
-    nena_gen_category_id = fields.Many2one("nena.gen.category", string="Area Operativa")
+    nena_gen_category_id = fields.Many2one("nena.gen.management", string="Area Operativa")
     job_ids = fields.One2many("hr.job", "nena_hr_job_id", string="Ocupaciones")
 
 
--- custom/nena_drug_composition/__manifest__.py
+++ custom/nena_drug_composition/__manifest__.py
@@ -1,14 +1,16 @@
 {
-    'name': 'Drug Composition',
-    'version': '18.0.0.0.1',
+    'name': 'Nena Drug Composition',
     'summary': 'Manage pharmaceutical compositions linked to products',
-    'description': """
+    'author': 'Droguería Nena, C.A.',
+    'category': 'Inventory',
+    'version': '18.0.0.0.1',
+    'application': False,
+    'description': 
+                """
                     Este módulo permite definir los principios activos y las composiciones 
                     de los productos farmacéuticos, vinculándolos directamente al modelo 
                     de producto nativo en Odoo.
-""",
-    'author': 'Angel Manzano',
-    'category': 'Inventory',
+                """,
     'depends': ['product', 'uom'],
     'data': [
         'security/ir.model.access.csv',
@@ -17,7 +19,4 @@
         'views/composition_views.xml',
         'views/prod_presentation_views.xml',
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
 }
